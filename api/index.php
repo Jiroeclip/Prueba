61e91cb4c013121465644861607ff04ad53df9f9
Ibanez <ibanezjosue907@gmail.com>
2024-08-27 09:29:19 -0400

    Conexion

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-$BDD=new mysqli("localhost","root","","pasajes");
+$BDD=new mysqli("localhost","root","redes2024","pasajes");
 
 $tam=strlen(dirname($_SERVER["SCRIPT_NAME"]));
 if($tam==1){$inc=0;}else{$inc=1;}
